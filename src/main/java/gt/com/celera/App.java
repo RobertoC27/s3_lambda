94c33871d73b46d2757531cb6a8459e4b6b0c1a4
RobertoC27 <chiroy_134@hotmail.com>
2020-03-16 13:07:22 -0600

    reupload file to dest bucket

--- src/main/java/gt/com/celera/App.java
+++ src/main/java/gt/com/celera/App.java
@@ -1,10 +1,17 @@
 package gt.com.celera;
 
+import java.io.InputStream;
+
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.amazonaws.services.lambda.runtime.events.S3Event;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
+import com.amazonaws.services.s3.model.GetObjectRequest;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.S3Object;
 
 public class App implements RequestHandler<S3Event, String> {
     LambdaLogger logger;
@@ -17,6 +24,17 @@ public class App implements RequestHandler<S3Event, String> {
 
         String srcKey = record.getS3().getObject().getUrlDecodedKey();
         logger.log(String.format("* Source object is %s", srcKey));
+
+        AmazonS3 s3Client = AmazonS3ClientBuilder.defaultClient();
+        S3Object s3Object = s3Client.getObject(new GetObjectRequest(srcBucket, srcKey));
+        
+        InputStream objectData = s3Object.getObjectContent();
+        logger.log("* uploading to dest bucket");
+        ObjectMetadata meta = new ObjectMetadata();
+        meta.addUserMetadata("test", "value");
+        String destBucket = "signed-pdfs";
+        s3Client.putObject(destBucket, srcKey, objectData,meta);
+        
         return "wenas";
     }
 
