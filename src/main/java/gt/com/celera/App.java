c5e5819d084fb9d1db3f07ae14649b16a9e4303e
RobertoC27 <chiroy_134@hotmail.com>
2020-03-16 12:09:37 -0600

    fixed import for S3Event

--- src/main/java/gt/com/celera/App.java
+++ src/main/java/gt/com/celera/App.java
@@ -3,20 +3,21 @@ package gt.com.celera;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.LambdaLogger;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
-import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.lambda.runtime.events.S3Event;
 import com.amazonaws.services.s3.event.S3EventNotification.S3EventNotificationRecord;
-import com.amazonaws.services.s3.model.S3Event;
 
 public class App implements RequestHandler<S3Event, String> {
     LambdaLogger logger;
 
-    public String handleRequest(S3Event s3Event, Context context)
-    logger = context.getLogger();
-    S3EventNotificationRecord record = s3Event.getRecords().get(0);
-    String srcBucket = record.getS3().getBucket().getName();
-    logger.log(String.format("* Source bucket is %s", srcBucket));
-    
-    String srcKey = record.getS3().getObject().getUrlDecodedKey();
-    logger.log(String.format("* Source object is %s", srcBucket));
+    public String handleRequest(S3Event s3Event, Context context) {
+        logger = context.getLogger();
+        S3EventNotificationRecord record = s3Event.getRecords().get(0);
+        String srcBucket = record.getS3().getBucket().getName();
+        logger.log(String.format("* Source bucket is %s", srcBucket));
+
+        String srcKey = record.getS3().getObject().getUrlDecodedKey();
+        logger.log(String.format("* Source object is %s", srcKey));
+        return "wenas";
+    }
 
 }
